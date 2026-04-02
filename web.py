cc2937dc5a94c2e4998b253119a110d7a57c77ea
ronaldreichmuth <ronald.reichmuth@icloud.com>
2026-04-02 20:27:50 +0200

    feat: adjust chart aspect ratio for improved layout

--- web.py
+++ web.py
@@ -58,6 +58,7 @@ HTML = """
                 },
                 plugins: { legend: { display: false } },
                 animation: false,
+                aspectRatio: 1.5,
             }
         });
 
